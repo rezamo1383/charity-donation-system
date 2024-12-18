0d44a4b9bfa1f2f3d956a136e03892d4d0f9d3b3
reza <rem67165@gmail.com>
2024-12-18 15:07:27 +0330

    connected to database

--- config/database.js
+++ config/database.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect('mongodb://localhost:27017/charitydonation', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err.message);
+        process.exit(1); // Exit with failure
+    }
 };
 
 module.exports = connectDB;
+
